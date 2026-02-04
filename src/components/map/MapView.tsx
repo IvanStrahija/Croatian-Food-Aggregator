e00332576fa11ddf2155f51d65e3f8f3778c817c
IvanStrahija <strahija9999@gmail.com>
2026-02-04 02:39:40 +0100

    Added carto map layer

--- src/components/map/MapView.tsx
+++ src/components/map/MapView.tsx
@@ -53,8 +53,8 @@ export function MapView({ markers = [] }: MapViewProps) {
       <div className="h-[32rem] w-full overflow-hidden rounded-lg">
         <MapContainer center={center} zoom={12} scrollWheelZoom className="h-full w-full">
           <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+            url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
           />
           {markers.map((marker) => (
             <Marker
